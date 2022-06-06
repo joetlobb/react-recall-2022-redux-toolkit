093d8c6cd89d16832817f445a0bdc8490c68d7a8
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-06 16:24:43 +0700

    assignment - using react-redux toolkit in other components for dispatching actions and retrieving states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Auth from "./components/Auth";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <React.Fragment>
       <Header />
-      <Auth />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       <Counter />
     </React.Fragment>
   );
